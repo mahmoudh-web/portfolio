815000839449aef80f3d3e4b0e4a712b24510eb0
Mahmoud Hassan <dev@interakt.dev>
2022-09-14 10:51:50 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,6 @@ export default function Home() {
     return (
         <div className="text-xl">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
         </div>
     )
 }
