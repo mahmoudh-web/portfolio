69a6111bbf04df13b3de4290f45128982165262d
Mahmoud Hassan <dev@interakt.dev>
2022-09-15 11:04:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function Home() {
 
     return (
         <div className="text-xl">
+            <p>first</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
@@ -28,6 +29,7 @@ export default function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto, autem nobis impedit beatae, ea corrupti quae incidunt esse culpa rerum architecto doloremque vero eveniet eos aspernatur ducimus ex ut!</p>
+            <p>last</p>
         </div>
     )
 }
