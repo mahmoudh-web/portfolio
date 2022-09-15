c207aec9baad096407956882d96fd4a85dfff999
Mahmoud Hassan <dev@interakt.dev>
2022-09-15 01:28:18 +0100

    /

---
+++ components/Template copy 2.js
@@ -0,0 +1,35 @@
+import { useAppContext } from '../AppContext'
+
+import Menu from '../components/Menu'
+import MenuBar from './MenuBar'
+
+const Template = ({children}) => {
+
+    const {width, height, mounted} = useAppContext()
+
+    if (!mounted) {
+        return null
+    }
+
+    return (
+        <div className={`
+            relative flex
+            overflow-hidden
+            h-[${height}px]
+            `}
+        >
+            <MenuBar />
+
+            <div className='flex flex-col w-full'>
+                <div className='bg-blue-200 h-12 pl-16'>Header Bar</div>
+                <div className='bg-blue-400 grow'>
+                    <p>Main Section 2</p>
+                    <p>{`${height} x ${width}`}</p>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Template
--- components/Template.js
+++ components/Template.js
@@ -1,33 +1,17 @@
 import { useAppContext } from '../AppContext'
-
-import Menu from '../components/Menu'
 import MenuBar from './MenuBar'
 
 const Template = ({children}) => {
 
-    const {width, height, mounted} = useAppContext()
+    const { height, mounted } = useAppContext()
 
-    if (!mounted) {
-        return null
-    }
-
+    const heightString = `${height}px`
     return (
-        <div className={`
-            relative flex
-            h-screen w-screen
-            overflow-hidden
-            `}
+        <div 
+            className="flex items-end w-full bg-red-200 p-8"
+            style={{height: heightString}}
         >
-            <MenuBar />
-
-            <div className='flex flex-col w-full'>
-                <div className='bg-blue-200 h-12 pl-16'>Header Bar</div>
-                <div className='bg-blue-400 grow'>
-                    <p>Main Section 2</p>
-                    <p>{`${height} x ${width}`}</p>
-                    
-                </div>
-            </div>
+            <p>test</p>
         </div>
     )
 }
