6166ddacbdb9e65fb5dee99f2e6df56104d4d562
Mahmoud Hassan <dev@interakt.dev>
2022-09-16 23:28:23 +0100

    Update index.js

--- pages/lab/index.js
+++ pages/lab/index.js
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react"
 import serverUrl from '../../serverConfig'
 
-// export const getStaticProps = async () => {
-//     const res = await fetch(`${serverUrl}/api/lab/allLab`)
-//     const data = await res.json()
+export const getStaticProps = async () => {
+    const res = await fetch(`${serverUrl}/api/lab/allLab`)
+    const data = await res.json()
 
-//     return {props: {data}}
-// }
+    return {props: {data}}
+}
 
 const Home = ({data}) => {
 
-    // const postsEl = data.map(post => {
+    const postsEl = data.map(post => {
 
-    //     const title = post.properties.Name.title[0].text.content
-    //     const url = post.url.replace('https://www.notion.so/', '')
+        const title = post.properties.Name.title[0].text.content
+        const url = post.url.replace('https://www.notion.so/', '')
 
-    //     return (
-    //         <div 
-    //             key={post.id}
-    //             className="cursor-pointer"
-    //         >
-    //             <p>{title}</p>
-    //             <p>{url}</p>
-    //         </div>
-    //     )
-    // })
+        return (
+            <div 
+                key={post.id}
+                className="cursor-pointer"
+            >
+                <p>{title}</p>
+                <p>{url}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
             <h2>Lab</h2>
-            {/* {postsEl} */}
+            {postsEl}
         </div>
     )
 }
