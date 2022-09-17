54c6d72a8df470bc1a28c4c95f75357a5a99a75e
Mahmoud Hassan <38259921+mahmoudh-web@users.noreply.github.com>
2022-09-17 12:58:21 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,8 @@ export default function Document() {
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@400;800&display=swap" rel="stylesheet" />        
+            <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@400;800&display=swap" rel="stylesheet" /
+            <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap" rel="stylesheet" />
         </ Head>
         <body>
             <Main />
