62b4ecd5ca4de4f06e428930f22f06328a755d99
Mahmoud Hassan <dev@interakt.dev>
2022-09-17 01:14:22 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NOTION_SECRET: process.env.NOTION_SECRET,
-    NOTION_DB_LAB: process.env.NOTION_DB_LAB
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
