d7740d9fd2862de8f64dc388a5b12d910793b6cf
Mahmoud Hassan <dev@interakt.dev>
2022-09-29 01:19:54 +0100

    Update index.js

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,11 +2,16 @@ import SimpleCard from '../../components/projects/SimpleCard'
 import projects from '../../components/homePage/sections/projects'
 
 const Projects = () => {
-    const projectsEl = projects.map((project, i) => <SimpleCard key={i} project={project} />)
+    const projectsEl = projects.map((project, i) => <div className="flex flex-col h-full w-full" key={i}><SimpleCard  project={project} /></div>)
 
     return ( 
-        <div className='grid grid-cols-3 gap-4'>
-            {projectsEl}
+        <div className='
+                container
+                sm:grid sm:grid-cols-2 sm:my-auto
+                flex flex-col gap-4
+            '
+        >
+                {projectsEl}
         </div>
     )
 }
