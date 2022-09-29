37c1f14ebae8a79a6f32d2f66be1b94ccb096e11
Mahmoud Hassan <dev@interakt.dev>
2022-09-29 01:00:34 +0100

    Update projects.js

--- components/homePage/sections/projects.js
+++ components/homePage/sections/projects.js
@@ -13,7 +13,7 @@ const projects = [
         title: 'Day #2: Random Password Generator',
         description: `A little utility that creates a random password. The user can select whether to use upper/lower case letters, numbers and symbols`,
         link: 'daytwo-random-password-generator',
-        github: 'https://github.com/mahmoudh-web/portfolio/blob/f528517ba49434c3bf46731501defc67ed921e9d/components/projects/01/CounterApp.js',
+        github: 'https://github.com/mahmoudh-web/portfolio/blob/1497dbc112d6e98ed6d203176fb0a6693fa87a17/components/projects/apps/PasswordGenerator.js',
         image: '',
         AppComponent: 'PasswordGenerator'
     },
