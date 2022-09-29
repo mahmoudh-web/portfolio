b599d7167a79d11c1049366d57a8ca843ba09f98
Mahmoud Hassan <dev@interakt.dev>
2022-09-29 01:02:44 +0100

    Update projects.js

--- components/homePage/sections/projects.js
+++ components/homePage/sections/projects.js
@@ -2,7 +2,7 @@ import CounterApp from "../../projects/apps/CounterApp"
 
 const projects = [
     {
-        title: 'Day #1: useState() 101: Counter',
+        title: 'Day #1 - useState() 101: Counter',
         description: `A simple counter app that allows you to increment/decrement the counter.`,
         link: 'dayone-usestate-counter',
         github: 'https://github.com/mahmoudh-web/portfolio/blob/f528517ba49434c3bf46731501defc67ed921e9d/components/projects/01/CounterApp.js',
@@ -10,7 +10,7 @@ const projects = [
         AppComponent: 'CounterApp'
     },
     {
-        title: 'Day #2: Random Password Generator',
+        title: 'Day #2 - Forms: Random Password Generator',
         description: `A little utility that creates a random password. The user can select whether to use upper/lower case letters, numbers and symbols`,
         link: 'daytwo-random-password-generator',
         github: 'https://github.com/mahmoudh-web/portfolio/blob/1497dbc112d6e98ed6d203176fb0a6693fa87a17/components/projects/apps/PasswordGenerator.js',
